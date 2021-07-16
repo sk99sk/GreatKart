7d536994095f0c20d362762512887b4aa8eebc4a
sk99sk <saksham70.sk@gmail.com>
2021-07-17 02:38:56 +0530

    completed cart functionality

--- carts/models.py
+++ carts/models.py
@@ -16,4 +16,7 @@ class CartItem(models.Model):
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.product
+        return self.product.product_name
+
+    def sub_total(self):
+        return (self.product.price * self.quantity)
