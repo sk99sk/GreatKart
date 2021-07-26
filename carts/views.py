cf3c1242b3e813dda4427df484ddfed904db1d7a
sk99sk <saksham70.sk@gmail.com>
2021-07-26 20:38:15 +0530

    correcting cart functionality

--- carts/views.py
+++ carts/views.py
@@ -67,27 +67,31 @@ def add_cart(request,product_id):
     return redirect('cart')
 
 
-def cart(request,total=0,quantity=0,cart_items=None):
+def cart(request, total=0, quantity=0, cart_items=None):
     try:
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart,is_active=True)
-        for cart_item in  cart_items:
+        tax = 0
+        grand_total = 0
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
-
-        tax = total*0.18
-        grand_total = total+tax
+        tax = (2 * total)/100
+        grand_total = total + tax
     except ObjectDoesNotExist:
-        pass
+        pass #just ignore
 
-    context ={
-        'total':total,
-        'quantity':quantity,
-        'cart_items':cart_items,
-        'tax':tax,
-        'grand_total':grand_total,
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax'       : tax,
+        'grand_total': grand_total,
     }
-    return render(request,'store/cart.html',context)
+    return render(request, 'store/cart.html', context)
 
 
 
